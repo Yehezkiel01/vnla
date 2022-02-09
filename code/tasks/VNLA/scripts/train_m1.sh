a73b28178daa2662a3d63282592c39adeafd3679
yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2022-02-10 04:00:37 +0800

    Omit unnecessary details from training scripts

--- code/tasks/VNLA/scripts/train_m1.sh
+++ code/tasks/VNLA/scripts/train_m1.sh
@@ -15,35 +15,6 @@ device=${2:-0}
 config_file="configs/verbal_hard.json"
 output_dir="main_$exp_name"
 
-extra=""
-
-if [ "$exp_name" == "none" ]
-then
-   extra="-no_ask 1"
-elif [ "$exp_name" == "first" ]
-then
-  extra="-ask_first 1"
-elif [ "$exp_name" == "random" ]
-then
-  extra="-random_ask 1"
-elif [ "$exp_name" == "teacher" ]
-then
-  extra="-teacher_ask 1"
-elif [ "$exp_name" == "learned" ]
-then
-  extra=""
-else
-  echo "Usage: bash train_m1.sh [none|first|random|teacher|learned] [gpu_id]"
-  echo "Example: bash train_m1.sh learned 0"
-  exit
-fi
-
-command="python -u m1_train.py -config $config_file -exp $output_dir $extra -device $device"
+command="python -u m1_train.py -config $config_file -exp $output_dir -device $device"
 echo $command
 $command
-
-
-
-
-
-
