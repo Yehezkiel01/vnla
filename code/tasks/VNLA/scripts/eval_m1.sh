5feddcd6030ed22a5f7ed0444bdbe245374f8c78
yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2022-02-24 12:59:56 +0800

    Fix eval scripts

--- code/tasks/VNLA/scripts/eval_m1.sh
+++ code/tasks/VNLA/scripts/eval_m1.sh
@@ -7,10 +7,16 @@ source define_vars.sh
 
 cd ../
 
-exp_type = $1
+exp_type=$1
 exp_name="m1-learned"
 split=$2
 device=${3:-0}
+
+config_file="configs/verbal_hard.json"
+output_dir="main_$exp_name"
+
+extra=""
+model_name="${output_dir}_nav_sample_ask_sample"
 
 if [ "$exp_type" == "none" ]
 then
@@ -30,12 +36,7 @@ else
   exit
 fi
 
-config_file="configs/verbal_hard.json"
-output_dir="main_$exp_name"
-
-model_name="${output_dir}_nav_sample_ask_sample"
-
-extra="-load_path $PT_OUTPUT_DIR/$model_name/${model_name}_val_${split}.ckpt -multi_seed 1 -success_radius 2"
+extra="$extra -load_path $PT_OUTPUT_DIR/$model_name/${model_name}_val_${split}.ckpt -multi_seed 1 -success_radius 2"
 
 command="time python -u m1_train.py -config $config_file -exp $output_dir $extra -device $device"
 echo $command
