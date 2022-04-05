322b83e74211610a5ba0567e8c53b0ab61ee841c
Yehezkiel Raymundo Theodoroes <39835686+Yehezkiel01@users.noreply.github.com>
2022-04-05 09:16:03 +0800

    Change m1 scripts to choose between question set 1 or 2 (#15)

--- code/tasks/VNLA/scripts/eval_m1.sh
+++ code/tasks/VNLA/scripts/eval_m1.sh
@@ -10,7 +10,8 @@ cd ../
 exp_type=$1
 exp_name="m1-learned"
 split=$2
-device=${3:-0}
+question_set=${3:-1}
+device=${4:-0}
 
 config_file="configs/verbal_hard.json"
 output_dir="main_$exp_name"
@@ -31,12 +32,24 @@ elif [ "$exp_type" == "rl" ]
 then
   extra=""
 else
-  echo "Usage: bash eval_m1.sh [rl|teacher|random|none] [seen|unseen] [gpu_id]"
-  echo "Example: bash eval_m1.sh rl seen 0"
+  echo "Usage: bash eval_m1.sh [rl|teacher|random|none] [seen|unseen] [question_set=1|2] [gpu_id]"
+  echo "Example: bash eval_m1.sh rl seen 2 0"
   exit
 fi
 
 extra="$extra -load_path $PT_OUTPUT_DIR/$model_name/${model_name}_val_${split}.ckpt -multi_seed 1 -success_radius 2"
+
+if [ $question_set == 1 ]
+then
+  extra="$extra -advisor verbal_qa"
+elif [ $question_set == 2 ]
+then
+  extra="$extra -advisor verbal_qa2"
+else
+  echo "Usage: bash eval_m1.sh [rl|teacher|random|none] [seen|unseen] [question_set=1|2] [gpu_id]"
+  echo "Example: bash eval_m1.sh rl seen 2 0"
+  exit
+fi
 
 command="time python -u m1_train.py -config $config_file -exp $output_dir $extra -device $device"
 echo $command
